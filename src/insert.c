198a88d75ed999ebffdc95b143ba4f33310825da
hack0303 <1097037585@qq.com>
2022-01-26 14:55:34 +0800

    .gitignore

--- src/insert.c
+++ src/insert.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-//插入排序
+//
 int* sortByUsingInsertSort(int* array,int len)
 {
-    int* after_sorted=malloc(len*sizeof(int));//内存分配
+    int* after_sorted=malloc(len*sizeof(int));
     for(int x=0; x<len; x++)
-    {//查看每一元素
-        int me=array[x];//当前参照元素
+    {
+        int me=array[x];
         int change_start=0;
-        short need_move=0;//
-        if(x==0){//第一个元素插入的情况
+        short need_move=0;
+        if(x==0){
         after_sorted[0]=me;
         continue;
         }
         for(int y=0; y<x; y++)
-        {//参照元素前面
+        {
             if(after_sorted[y]>=me)
             {
                 need_move=1;
-              //printf("need move\n");
                 change_start=y;
-                break;//找到一个就停止后续比较
+                break;
             }
         }
         if(!need_move){
@@ -28,7 +27,7 @@ int* sortByUsingInsertSort(int* array,int len)
             continue;
         }
             for(int z=x; z>change_start; z--)
-        {//依次顺移
+        {
             after_sorted[z]=after_sorted[z-1];
         }
         after_sorted[change_start]=me;
